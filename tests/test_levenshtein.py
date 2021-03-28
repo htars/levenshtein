f46d434ada23d01d9084a9a26de94cb66c67c749
Yohei Hiyama <hymyhds@gmail.com>
2021-03-28 13:49:42 +0900

    add exact mismatch case

--- tests/test_levenshtein.py
+++ tests/test_levenshtein.py
@@ -10,3 +10,10 @@ class LevenshteinTest(unittest.TestCase):
         expected = 1.0
         actual = Levenshtein.calc_similarity(s, t)
         self.assertEqual(expected, actual)
+
+    def test_levenshtein_exact_mismatch(self):
+        s = 'aaa'
+        t = 'bbb'
+        expected = 0.0
+        actual = Levenshtein.calc_similarity(s, t)
+        self.assertEqual(expected, actual)        
